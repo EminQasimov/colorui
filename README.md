1db15ab260fe784cf945698497a974a571fefa47
EminQasimov <eminqasimov@outlook.com>
2019-05-11 02:29:14 +0400

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-### ⭐ [ design is taken from  @lubosvolkov](https://www.instagram.com/p/BuBhhSPBEjl/)
-<img src="https://i.postimg.cc/cshfC9yp/localhost-3000-www-coolors-co.png" width="777px" title="full">
---
+++ precache-manifest.e919f3f69383ad08f2f3118fd82e8d14.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f97b01a565b4154ecf8956aa0f4e6981",
+    "url": "/colorui/index.html"
+  },
+  {
+    "revision": "b85caeb855f0bbbada0d",
+    "url": "/colorui/static/css/main.edf5d1d6.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/colorui/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "b85caeb855f0bbbada0d",
+    "url": "/colorui/static/js/main.2c091d69.chunk.js"
+  },
+  {
+    "revision": "5eda8489a98d6fbf85e4",
+    "url": "/colorui/static/js/runtime~main.10c09e4b.js"
+  },
+  {
+    "revision": "7d354041aab64798ede99d555983d0b3",
+    "url": "/colorui/static/media/filters.7d354041.svg"
+  }
+]);
